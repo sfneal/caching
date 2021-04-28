9f9afe520ccde5d57f202f19f71b1bf2c0671479
Stephen Neal <stephen.neal14@gmail.com>
2021-04-28 13:33:18 -0400

    ADD use of `josiasmontag/laravel-redis-mock` for redis mocking

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,26 @@
 namespace Sfneal\Caching\Tests;
 
 use Illuminate\Foundation\Application;
+use Lunaweb\RedisMock\Providers\RedisMockServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Sfneal\Helpers\Redis\Providers\RedisHelpersServiceProvider;
 
 class TestCase extends OrchestraTestCase
 {
+    /**
+     * Define environment setup.
+     *
+     * @param Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.debug', true);
+        $app['config']->set('database.redis.client', 'mock');
+        $app['config']->set('cache.default', 'redis');
+        $app['config']->set('cache.prefix', 'redis-helpers');
+    }
+
     /**
      * Register package service providers.
      *
@@ -18,6 +33,7 @@ class TestCase extends OrchestraTestCase
     {
         return [
             RedisHelpersServiceProvider::class,
+            RedisMockServiceProvider::class,
         ];
     }
 }
