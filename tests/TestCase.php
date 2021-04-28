5b610f8d8077ce12203bd82630d31fb3838d4905
Stephen Neal <stephen.neal14@gmail.com>
2021-04-28 13:16:15 -0400

    CUT repeated test method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,7 @@ namespace Sfneal\Caching\Tests;
 
 use Illuminate\Foundation\Application;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Sfneal\Caching\Tests\Mocks\TodaysDateHash;
 use Sfneal\Helpers\Redis\Providers\RedisHelpersServiceProvider;
-use Sfneal\Helpers\Redis\RedisCache;
 
 class TestCase extends OrchestraTestCase
 {
@@ -19,16 +17,5 @@ class TestCase extends OrchestraTestCase
     protected function getPackageProviders($app)
     {
         return RedisHelpersServiceProvider::class;
-    }
-
-    public function test_caching_is_working()
-    {
-        $todaysDate = new TodaysDateHash();
-        $key = $todaysDate->cacheKey();
-
-        $output = $todaysDate->fetch();
-
-        $this->assertTrue($todaysDate->isCached());
-        $this->assertTrue(RedisCache::get($key) == $output);
     }
 }
