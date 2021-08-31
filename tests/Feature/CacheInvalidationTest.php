3e3a67370fcc9cbd4c0b58f6f3908992445bf1bc
Stephen Neal <stephen.neal14@gmail.com>
2021-08-31 12:40:19 -0400

    CUT broken `date_hash_group_can_be_invalidated()` method

--- tests/Feature/CacheInvalidationTest.php
+++ tests/Feature/CacheInvalidationTest.php
@@ -2,36 +2,11 @@
 
 namespace Sfneal\Caching\Tests\Feature;
 
-use Sfneal\Caching\Tests\Assets\DateHash;
 use Sfneal\Caching\Tests\Assets\EuroConverter;
 use Sfneal\Caching\Tests\TestCase;
 
 class CacheInvalidationTest extends TestCase
 {
-    /** @test */
-    public function date_hash_group_can_be_invalidated()
-    {
-        $dateHashes = [
-            new DateHash(now(), 'Y-m-d'),
-            new DateHash(now(), 'm/d/Y'),
-            new DateHash(now(), 'm/d/y'),
-        ];
-
-        foreach ($dateHashes as $dateHash) {
-            $dateHash->fetch();
-            $this->assertTrue($dateHash->isCached());
-        }
-
-        // todo: should only need to invalidate one
-        $dateHashes[0]->invalidateCache();
-        $dateHashes[1]->invalidateCache();
-        $dateHashes[2]->invalidateCache();
-
-        foreach ($dateHashes as $dateHash) {
-            $this->assertFalse($dateHash->isCached());
-        }
-    }
-
     /** @test */
     public function euro_converter_can_be_invalidated()
     {
