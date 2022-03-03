fb1fa3fce30590c7d401108f2c2b2f591299f3f0
Stephen Neal <stephen.neal14@gmail.com>
2022-03-03 15:19:31 -0500

    Apply fixes from StyleCI (#28)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,7 +18,7 @@ class TestCase extends OrchestraTestCase
     /**
      * Define environment setup.
      *
-     * @param Application $app
+     * @param  Application  $app
      * @return void
      */
     protected function getEnvironmentSetUp($app)
@@ -41,7 +41,7 @@ class TestCase extends OrchestraTestCase
     /**
      * Register package service providers.
      *
-     * @param Application $app
+     * @param  Application  $app
      * @return array|string
      */
     protected function getPackageProviders($app)
